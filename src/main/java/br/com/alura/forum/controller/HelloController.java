f953ff159fb67fa666c223ea0e14ccb949edb7bf
IlaMuniz <ilammuniz@gmail.com>
2021-08-31 22:53:37 -0300

    feat: adição da dependência DevTools

--- src/main/java/br/com/alura/forum/controller/HelloController.java
+++ src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -1,14 +1,15 @@
 package br.com.alura.forum.controller;
 
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController    //anotação RestController ao invés de Controller para não precisar usar o ResponseBody
 public class HelloController {
 
     @RequestMapping("/")     //indica qual URL é para chamar
-    @ResponseBody            //para devolver a string direto para o navegador, ao invés de procurar uma página com o nome da string
+    //@ResponseBody            //para devolver a string direto para o navegador, ao invés de procurar uma página com o nome da string
     public String hello() {
         return "Hello World!";
     }
